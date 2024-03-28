f51c93998ceaae4cfb6beb52216f17abec48b8b4
Dev-vinixx <viniciuseduardo0500@gmail.com>
2024-03-28 02:37:08 -0300

    first commit i made the header and a img of background and now i making up for GitHub

--- src/Main.jsx
+++ src/Main.jsx
@@ -18,24 +18,20 @@ function Main() {
     };
   }, []);
 
-  // Limites para o movimento da imagem
   const limite = 3;
 
-  // Posição inicial da imagem em porcentagem da largura/altura da janela
   const imgInitialPos = { x: -50, y: -50 };
 
-  // Calcula a posição da imagem com base na posição do mouse
   const imgPos = {
     x: imgInitialPos.x - limite / 0.5 + mousePos.x / window.innerWidth * limite,
     y: imgInitialPos.y - limite / 0.5 + mousePos.y / window.innerHeight * limite
   };
 
-  // Cria a animação
   const props = useSpring({ transform: `translate(${imgPos.x}%, ${imgPos.y}%)` });
 
   return (
     <main>
-      <animated.img className="imgBackground" src="./assets/react.svg" alt="" style={props} />
+      <animated.img className="imgBackground" src="../assets/react.svg" alt="" style={props} />
       <Header />
     </main>
   );
