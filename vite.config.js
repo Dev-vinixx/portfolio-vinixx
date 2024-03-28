66490964c759c7aa47e063021a80cdafe4696497
Dev-vinixx <viniciuseduardo0500@gmail.com>
2024-03-28 20:48:01 -0300

    deploy for Git Hub Pages two

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Dev-vinixx/portfolio-vinixx"
+  base: "/Dev-vinixx/portfolio-vinixx",
 })
