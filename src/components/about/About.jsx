a252c3e561076c3936799a157e82dc4afd5ab53a
Dev-vinixx <viniciuseduardo0500@gmail.com>
2024-04-20 04:08:38 -0300

    Cleaning the shelter area to start the work

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,8 +4,6 @@ function About () {
 
  return (
   <section className={styles.about}>
-   <img src="src\assets\under-construction\robin.jpg" alt="" />
-   <p>I apologize, this page is still under construction.</p>
   </section>
  )
 }
